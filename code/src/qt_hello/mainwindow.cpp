747bef65459099af8be782582b5294f6106bab37
Georgii Zhulikov <l8doku@gmail.com>
2022-05-12 14:34:36 +0300

    W31

--- code/src/qt_hello/mainwindow.cpp
+++ code/src/qt_hello/mainwindow.cpp
@@ -5,13 +5,20 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    
+    
     ui->setupUi(this);
+    
     variable = 10;
     QString buttonText = ui->pushButton->text();
     
     ui->pushButton->setText("Click me!");
+    
+    
     QFont curFont = ui->label->font();
-    curFont.setPointSize(10);
+    
+    curFont.setPointSize(100);
+    
     ui->label->setFont(curFont);
 }
 
